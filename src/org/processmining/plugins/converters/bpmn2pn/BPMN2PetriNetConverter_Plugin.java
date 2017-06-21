fc571fc11d9859f5d1d7635f883e4fdf7926b178
hverbeek <hverbeek@a68f48d4-8d00-424f-b812-3f22a534a02a>
2017-06-21 09:31:10 +0000

    Made the two BPMN-to-(Data)PetriNet converters PeerReviewed. As a result, they will show in ProM Lite.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/BPMNConversions/Trunk@35927 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/converters/bpmn2pn/BPMN2PetriNetConverter_Plugin.java
+++ src/org/processmining/plugins/converters/bpmn2pn/BPMN2PetriNetConverter_Plugin.java
@@ -8,6 +8,7 @@ import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.Progress;
 import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginLevel;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.models.connections.petrinets.behavioral.InitialMarkingConnection;
 import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
@@ -15,37 +16,37 @@ import org.processmining.models.graphbased.directed.petrinet.Petrinet;
 import org.processmining.models.semantics.petrinet.Marking;
 
 /**
- * Conversion of a BPMN model to Petri nets, only considering the control-flow of the model. 
- *
- * @author Dirk Fahland
- * Jul 18, 2013
+ * Conversion of a BPMN model to Petri nets, only considering the control-flow
+ * of the model.
+ * 
+ * @author Dirk Fahland Jul 18, 2013
  */
-@Plugin(name = "Convert BPMN diagram to Petri net (control-flow)", parameterLabels = { "BPMN model", "Conversion Configuration" }, 
-returnLabels = { "Petri net", "Initial Marking"}, returnTypes = { Petrinet.class, Marking.class }, 
-userAccessible = true, help = "Convert BPMN diagram to Petri net")
+@Plugin(name = "Convert BPMN diagram to Petri net (control-flow)", level = PluginLevel.PeerReviewed, parameterLabels = {
+		"BPMN model", "Conversion Configuration" }, returnLabels = { "Petri net", "Initial Marking" }, returnTypes = {
+		Petrinet.class, Marking.class }, userAccessible = true, help = "Convert BPMN diagram to Petri net")
 public class BPMN2PetriNetConverter_Plugin {
-	
+
 	@UITopiaVariant(affiliation = "TU/e", author = "D. Fahland", email = "d.fahland@tue.nl")
 	@PluginVariant(variantLabel = "Convert BPMN diagram to Petri net", requiredParameterLabels = { 0 })
 	public Object[] convert(UIPluginContext context, BPMNDiagram bpmn) {
-		BPMN2PetriNetConverter_Configuration config = new BPMN2PetriNetConverter_Configuration();		
+		BPMN2PetriNetConverter_Configuration config = new BPMN2PetriNetConverter_Configuration();
 		BPMN2PetriNetConverter_UI ui = new BPMN2PetriNetConverter_UI(config);
 		if (ui.setParameters(context, config) != InteractionResult.CANCEL)
 			return convert(context, bpmn, config);
 		else
 			return cancel(context, "Cancelled by user.");
 	}
-	
+
 	@PluginVariant(variantLabel = "Convert BPMN diagram to Petri net", requiredParameterLabels = { 0, 1 })
 	public Object[] convert(PluginContext context, BPMNDiagram bpmn, BPMN2PetriNetConverter_Configuration config) {
-		
+
 		BPMN2PetriNetConverter conv = new BPMN2PetriNetConverter(bpmn, config);
-		
+
 		Progress progress = context.getProgress();
 		progress.setCaption("Converting BPMN diagram to Petri net");
-		
+
 		boolean success = conv.convert();
-		
+
 		if (success) {
 			Petrinet net = conv.getPetriNet();
 			Marking m = conv.getMarking();
@@ -53,30 +54,32 @@ public class BPMN2PetriNetConverter_Plugin {
 
 			if (!conv.getWarnings().isEmpty())
 				showWarningsandErrors(context, conv);
-			
-			context.getFutureResult(0).setLabel("Petri net from "+bpmn.getLabel());
-			context.getFutureResult(1).setLabel("Initial marking of the PN from "+bpmn.getLabel());
-		
-			return new Object[] {net, m};
+
+			context.getFutureResult(0).setLabel("Petri net from " + bpmn.getLabel());
+			context.getFutureResult(1).setLabel("Initial marking of the PN from " + bpmn.getLabel());
+
+			return new Object[] { net, m };
 		} else {
 			if (!conv.getErrors().isEmpty() || !conv.getWarnings().isEmpty())
 				showWarningsandErrors(context, conv);
-			
+
 			return cancel(context, "Could not translate BPMN diagram");
 		}
 	}
-	
+
 	private void showWarningsandErrors(PluginContext context, BPMN2PetriNetConverter conv) {
 		StringBuffer warnings = new StringBuffer();
 		for (String error : conv.getErrors()) {
-			warnings.append("Error: "+error); warnings.append('\n');
+			warnings.append("Error: " + error);
+			warnings.append('\n');
 		}
 		for (String warning : conv.getWarnings()) {
-			warnings.append("Warning: "+warning); warnings.append('\n');
+			warnings.append("Warning: " + warning);
+			warnings.append('\n');
 		}
 		showMessage(context, warnings.toString());
 	}
-	
+
 	private void showMessage(PluginContext context, String message) {
 		if (context instanceof UIPluginContext) {
 			JOptionPane.showMessageDialog(null, message, "BPMN2PetriNet conversion", JOptionPane.WARNING_MESSAGE);
@@ -85,9 +88,9 @@ public class BPMN2PetriNetConverter_Plugin {
 			context.log(message);
 		}
 	}
-	
+
 	public static Object[] cancel(PluginContext context, String message) {
-		System.out.println("[BPMN2PetriNet]: "+message);
+		System.out.println("[BPMN2PetriNet]: " + message);
 		context.log(message);
 		context.getFutureResult(0).cancel(true);
 		context.getFutureResult(1).cancel(true);
--- src/org/processmining/plugins/converters/bpmn2pn/BPMN2PetriNetWithDataConverter_Plugin.java
+++ src/org/processmining/plugins/converters/bpmn2pn/BPMN2PetriNetWithDataConverter_Plugin.java
@@ -9,47 +9,44 @@ import org.processmining.datapetrinets.DataPetriNet;
 import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.Progress;
 import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginLevel;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.models.connections.petrinets.behavioral.InitialMarkingConnection;
 import org.processmining.models.graphbased.directed.bpmn.BPMNDiagram;
 import org.processmining.models.semantics.petrinet.Marking;
 
 /**
- * Conversion of a BPMN model to Petri nets, only considering the control-flow of the model. 
- *
- * @author Dirk Fahland
- * Jul 18, 2013
+ * Conversion of a BPMN model to Petri nets, only considering the control-flow
+ * of the model.
+ * 
+ * @author Dirk Fahland Jul 18, 2013
  */
-@Plugin(name = "Convert BPMN diagram to Data Petri net", 
-parameterLabels = { "BPMN model", "Conversion Configuration" }, returnLabels = { "Data Petri net", "Initial Marking"}, 
-returnTypes = { DataPetriNet.class, Marking.class }, 
-userAccessible = true, help = "Convert BPMN diagram to Data Petri net")
+@Plugin(name = "Convert BPMN diagram to Data Petri net", level = PluginLevel.PeerReviewed, parameterLabels = {
+		"BPMN model", "Conversion Configuration" }, returnLabels = { "Data Petri net", "Initial Marking" }, returnTypes = {
+		DataPetriNet.class, Marking.class }, userAccessible = true, help = "Convert BPMN diagram to Data Petri net")
 public class BPMN2PetriNetWithDataConverter_Plugin {
-	
-	
-	
-	@UITopiaVariant(affiliation = "HSE,TU/e", author = "A. Kalenkova, D. Fahland", 
-			email = "akalenkova@hse.ru, d.fahland@tue.nl")
+
+	@UITopiaVariant(affiliation = "HSE,TU/e", author = "A. Kalenkova, D. Fahland", email = "akalenkova@hse.ru, d.fahland@tue.nl")
 	@PluginVariant(variantLabel = "Convert BPMN diagram to Data Petri net", requiredParameterLabels = { 0 })
 	public Object[] convert(UIPluginContext context, BPMNDiagram bpmn) {
-		BPMN2PetriNetWithDataConverter_Configuration config = new BPMN2PetriNetWithDataConverter_Configuration();		
+		BPMN2PetriNetWithDataConverter_Configuration config = new BPMN2PetriNetWithDataConverter_Configuration();
 		BPMN2PetriNetWithDataConverter_UI ui = new BPMN2PetriNetWithDataConverter_UI(config);
 		if (ui.setParameters(context, config) != InteractionResult.CANCEL)
 			return convert(context, bpmn, config);
 		else
 			return cancel(context, "Cancelled by user.");
 	}
-	
+
 	@PluginVariant(variantLabel = "Convert BPMN diagram to Data Petri net", requiredParameterLabels = { 0, 1 })
 	public Object[] convert(PluginContext context, BPMNDiagram bpmn, BPMN2PetriNetWithDataConverter_Configuration config) {
-		
+
 		BPMN2DataPetriNetConverter conv = new BPMN2DataPetriNetConverter(bpmn, config);
-		
+
 		Progress progress = context.getProgress();
 		progress.setCaption("Converting BPMN diagram to Data Petri net");
-		
+
 		boolean success = conv.convertWithData();
-		
+
 		if (success) {
 			DataPetriNet net = conv.getDataPetriNet();
 			Marking m = conv.getMarking();
@@ -57,30 +54,32 @@ public class BPMN2PetriNetWithDataConverter_Plugin {
 
 			if (!conv.getWarnings().isEmpty())
 				showWarningsandErrors(context, conv);
-			
-			context.getFutureResult(0).setLabel("Data Petri net from "+bpmn.getLabel());
-			context.getFutureResult(1).setLabel("Initial marking of the PN from "+bpmn.getLabel());
-		
-			return new Object[] {net, m};
+
+			context.getFutureResult(0).setLabel("Data Petri net from " + bpmn.getLabel());
+			context.getFutureResult(1).setLabel("Initial marking of the PN from " + bpmn.getLabel());
+
+			return new Object[] { net, m };
 		} else {
 			if (!conv.getErrors().isEmpty() || !conv.getWarnings().isEmpty())
 				showWarningsandErrors(context, conv);
-			
+
 			return cancel(context, "Could not translate BPMN diagram");
 		}
 	}
-	
+
 	private void showWarningsandErrors(PluginContext context, BPMN2PetriNetConverter conv) {
 		StringBuffer warnings = new StringBuffer();
 		for (String error : conv.getErrors()) {
-			warnings.append("Error: "+error); warnings.append('\n');
+			warnings.append("Error: " + error);
+			warnings.append('\n');
 		}
 		for (String warning : conv.getWarnings()) {
-			warnings.append("Warning: "+warning); warnings.append('\n');
+			warnings.append("Warning: " + warning);
+			warnings.append('\n');
 		}
 		showMessage(context, warnings.toString());
 	}
-	
+
 	private void showMessage(PluginContext context, String message) {
 		if (context instanceof UIPluginContext) {
 			JOptionPane.showMessageDialog(null, message, "BPMN2DataPetriNet conversion", JOptionPane.WARNING_MESSAGE);
@@ -89,9 +88,9 @@ public class BPMN2PetriNetWithDataConverter_Plugin {
 			context.log(message);
 		}
 	}
-	
+
 	public static Object[] cancel(PluginContext context, String message) {
-		System.out.println("[BPMN2DataPetriNet]: "+message);
+		System.out.println("[BPMN2DataPetriNet]: " + message);
 		context.log(message);
 		context.getFutureResult(0).cancel(true);
 		context.getFutureResult(1).cancel(true);
